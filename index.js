879139a7d4bfabfb4bc900868960ae8cd16bd1f1
Olivia Gilliam <oagilliam@outlook.com>
2023-09-10 17:07:15 -0400

    database added

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+// Connections to the database (Google firebase)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://endorsements-app-78379-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const endorsementsInDB = ref(database, "endorsements");
+
 const publishBtn = document.getElementById('publish-button');
+const inputField = document.getElementById('endorsement-field');
+
 
 publishBtn.addEventListener("onclick", function() {
-    
+    let inputValue = inputField.vslue;
+    push(endorsementsInDB, inputValue);
+
+
+
+
 })
